407fda37aef4fa0dcbc339f588b11f9cfa2f1916
Lucas Blante <lucasviniciusblante@gmail.com>
2023-08-11 22:55:46 -0300

    FEAT: adding message sending functionality

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { Genos } from 'next/font/google'
+import { PaperPlaneTilt } from "@phosphor-icons/react";
+
+const genos = Genos({ subsets: ['latin'] })
+
+export default function Header() {
+  return (
+    <header className="flex flex-col gap-6 md:h-fit md:mr-8 text-grey-100 max-w-sm">
+    <div className="flex flex-row gap-2 mt-4 items-center">
+      <PaperPlaneTilt size={30} weight='fill' className='md:h-10 h-8 w-auto' />
+      <strong
+        className={`${genos.className} font-medium text-5xl `}
+      >
+        Direct Whatsapp
+      </strong>
+    </div>
+    <span className="text-lg md:text-xl mb-10 font-regular ">
+      Envie mensagens para números no Whatsapp sem a necessidade de
+      adicionar aos contatos.
+    </span>
+  </header>
+  )
+}
